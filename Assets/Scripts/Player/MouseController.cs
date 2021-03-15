330170bc8f8d463a4b948d36382225e5d88bdd1f
Joshua Carney <48502124+jkcarney@users.noreply.github.com>
2021-03-15 16:44:55 -0400

    Added movable arm/plate

---
+++ Assets/Scripts/Player/MouseController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    public float plateDistance = 10f;
+
+    void Start()
+    {
+        // When the game begins we want the cursor to vanish
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        //Offset plate by an adjustable distance so it's far from the camera.
+        mousePos.z += plateDistance;
+        //Set plate to wherever the mouse is.
+        this.transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+    }
+}
