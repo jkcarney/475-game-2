2a56c333f522efb5f86fc6c523b382ce76977120
avedwo159 <80016982+avedwo159@users.noreply.github.com>
2021-03-24 11:02:23 -0400

    Added bad item spawn & increased spawn chance for item needed
    touched main

--- Assets/Scripts/Spawning/Spawning.cs
+++ Assets/Scripts/Spawning/Spawning.cs
@@ -1,28 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Spawning : MonoBehaviour
 {
 
     //introduce all ingredients
-    public GameObject TopBun;
-    public GameObject BottomBun;
-    public GameObject Patty;
-    //public GameObject Lettuce;
-    public GameObject Cheese;
+    public GameObject[] ingredients;
+    public GameObject[] badIngredients;
     private GameObject food;
+    private string[] ingredientNames;
+    private OrderManager orderManager;
 
     //variables for spawning food
     public float spawnRate = 1f; //change the speed that food spawns
-    private float lastSpawned;
+    private float lastSpawned; //used for creating the delay in spawning the food
+    public float graceChance = 50f; //chance for spawning food item that player currently need (out of 100)
 
+    
+    void Start()
+    {
+        ingredientNames = new string[ingredients.Length];
+        orderManager = GameObject.Find("OrderManager").GetComponent<OrderManager>(); //initialize ordermanager to be referenced
+        for (int i=0; i<ingredients.Length; i++) //fill array full of strings of ingredient names
+        {
+            ingredientNames[i] = ingredients[i].name;
+        }
+    }
     //spawns food at Random X between -10 and 10, 30 Y, and -25 Z
     void Spawn(GameObject prefab)
     {
-        Instantiate(prefab, new Vector3(Random.Range(-10.0f, 10.0f), 30.0f, -25.0f), Quaternion.identity);
+        Instantiate(prefab, new Vector3(UnityEngine.Random.Range(-10.0f, 10.0f), 30.0f, -25.0f), Quaternion.identity);
     }
-    
+
+    //same method as spawn but used to spawn bad food (work around to spawn bad food above normal food)
+    void BadSpawn(GameObject prefab)
+    {
+        Instantiate(prefab, new Vector3(UnityEngine.Random.Range(-10.0f, 10.0f), 32.0f, -25.0f), Quaternion.identity);
+    }
+
     //destroys food on collision with the DestroyPlane
     void OnTriggerEnter(Collider other)
     {
@@ -33,21 +50,25 @@ public class Spawning : MonoBehaviour
     void Update()
     {
         //randomly selecting which ingredient to spawn
-        var randomNumber = Random.Range(1, 6);
-        if (randomNumber == 1) food = TopBun;
-        else if (randomNumber == 2) food = BottomBun;
-        else if (randomNumber == 3) food = Patty;
-        //else if (randomNumber == 4) food = Lettuce;
-        else if (randomNumber == 5) food = Cheese;
-
-
+        var randomNumber = UnityEngine.Random.Range(0, ingredients.Length);
+        var randomNumber2 = UnityEngine.Random.Range(1, 100);
+        var randomNumber3 = UnityEngine.Random.Range(0, badIngredients.Length);
+        
         //process for creating the delay in spawning the food
         if (Time.time > lastSpawned + spawnRate)
         {
-            Spawn(food);
+            Spawn(ingredients[randomNumber]);
+
+            //roll chance for spawning food item that you need
+            if (randomNumber2 < graceChance)
+                Spawn(ingredients[Array.IndexOf(ingredientNames, orderManager.GetNextExpectedItem())]);
+
+            //roll chance for spawning bad item
+            if (randomNumber2 < DifficultyStatic.trashChance)
+                BadSpawn(badIngredients[randomNumber3]);
+
             lastSpawned = Time.time;
         }
-        
 
     }
 }
