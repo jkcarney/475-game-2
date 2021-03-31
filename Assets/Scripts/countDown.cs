8aee9b560611238aac29c210fc64d681ffec47ce
Joshua Carney <48502124+jkcarney@users.noreply.github.com>
2021-03-31 09:34:11 -0400

    Timer sfx when timer is <5 seconds

--- Assets/Scripts/countDown.cs
+++ Assets/Scripts/countDown.cs
@@ -9,17 +9,29 @@ public class countDown : MonoBehaviour
     
     public float timeStart = 60;
     public Text textBox;
+
+    private AudioSource timerTick;
+    private float timerTickInterval = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
         textBox.text = timeStart.ToString();
+        timerTick = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(timeStart <= 5f && timerTickInterval <= 0.0f)
+        {
+            timerTick.Play();
+            timerTickInterval = 1.0f;
+        }
+
         textBox.text = timeStart.ToString();
         timeStart -= Time.deltaTime;
+        timerTickInterval -= Time.deltaTime;
         textBox.text = Mathf.Round(timeStart).ToString();
         if(timeStart <= 0){
             timeStart = 0;
